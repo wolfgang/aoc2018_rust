7d7e78730e3051ba59586f0d30f27eff003d1b2e
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-04 21:23:19 +0100

    Name entries correctly

---
+++ aoc_1.2/src/main.rs
@@ -0,0 +1,50 @@
+
+use std::io::{self, BufReader};
+use std::io::prelude::*;
+use std::fs::File;
+
+use std::collections::HashSet;
+
+
+fn main() -> io::Result<()> {
+
+    let mut offsets = vec![];
+
+    let f = File::open("input.txt")?;
+    let f = BufReader::new(f);
+
+    for line in f.lines() {
+        let line = line.unwrap();
+        let i = line.parse::<i32>().unwrap();
+        offsets.push(i);
+    }
+
+    let mut sum = 0;
+    let mut freqs = HashSet::new();
+    let mut dup_found = false;
+    let mut iterations = 0;
+
+    while !dup_found {
+        iterations += 1;
+        for i in &offsets {
+            sum += i;
+            if freqs.contains(&sum) {
+                dup_found = true;
+                println!("First dup: {} Iterations: {}", sum, iterations);
+                break;
+            }
+            freqs.insert(sum);          
+        }
+
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn can_assert() {
+        assert_eq!(1, 1);
+    }
+}
--- aoc_2/src/main.rs
+++ aoc_2/src/main.rs
@@ -1,50 +1,122 @@
-
-use std::io::{self, BufReader};
+use std::io::{BufReader};
 use std::io::prelude::*;
 use std::fs::File;
 
-use std::collections::HashSet;
+use std::collections::HashMap;
 
+fn main()  {
+    let ids = load_ids();
+    let check_sum = calculate_checksum(&ids);
+    let common_chars = get_common_chars(&ids);
 
-fn main() -> io::Result<()> {
+    println!("Checksum: {}", check_sum);
+    println!("Common chars: {}", common_chars);
+}
 
-    let mut offsets = vec![];
+fn load_ids() -> Vec<String> {
+    let mut ids = vec![];
 
-    let f = File::open("input.txt")?;
+    let f = File::open("input.txt").expect("Failed to open input.txt");
     let f = BufReader::new(f);
 
     for line in f.lines() {
         let line = line.unwrap();
-        let i = line.parse::<i32>().unwrap();
-        offsets.push(i);
+        ids.push(line);
     }
 
-    let mut sum = 0;
-    let mut freqs = HashSet::new();
-    let mut dup_found = false;
-    let mut iterations = 0;
-
-    while !dup_found {
-        iterations += 1;
-        for i in &offsets {
-            sum += i;
-            if freqs.contains(&sum) {
-                dup_found = true;
-                println!("First dup: {} Iterations: {}", sum, iterations);
-                break;
-            }
-            freqs.insert(sum);          
-        }
-
-    }
-
-    Ok(())
+    return ids;
 }
+
+fn calculate_checksum(ids: &Vec<String>) -> i32 {
+    let mut count_with_two = 0;
+    let mut count_with_three = 0;
+
+    for id in ids {
+        if contains_two_of_any_letter(&id) {
+            count_with_two += 1
+        }
+        if contains_three_of_any_letter(&id) {
+            count_with_three += 1
+        }
+    }
+
+    return count_with_two*count_with_three;
+}
+
+fn get_common_chars(ids: &Vec<String>) -> String {
+    for (i, id) in ids.iter().enumerate() {
+        for i2 in i+1 .. ids.len() {
+            let common = common_chars(&id, &ids[i2]);
+            if common.len() == id.len() - 1 {
+                return common;
+            }
+        }
+    }
+
+    return String::from("")
+}
+
+fn contains_two_of_any_letter(s: &String) -> bool {
+    return contains_any_letter_times(2, s);
+}
+
+fn contains_three_of_any_letter(s: &String) -> bool {
+    return contains_any_letter_times(3, s);
+}
+
+fn contains_any_letter_times(wanted_count: i32, s: &String) -> bool {
+    let mut counts = HashMap::new();
+    for c in s.chars() { 
+        if !counts.contains_key(&c) {
+            counts.insert(c, 0);
+        }
+        let count = counts.get_mut(&c).unwrap();
+        *count += 1;
+    }
+    
+    for (_, count) in &counts {
+        if *count == wanted_count {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+fn common_chars(s1: &String, s2: &String) -> String {
+    let mut result = String::from("");
+    for (i, c) in s1.chars().enumerate() {
+        if c == s2.chars().nth(i).unwrap() {
+            result.push(c);
+        }
+    }
+    return result;
+}
+
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn can_assert() {
-        assert_eq!(1, 1);
+    fn contains_two_of_any_letter_() {
+        assert!(!contains_two_of_any_letter(&String::from("abc")));
+        assert!(contains_two_of_any_letter(&String::from("aac")));
+        assert!(contains_two_of_any_letter(&String::from("vinihorkulbfedcyzmsqgdxpau")));
+        assert!(!contains_two_of_any_letter(&String::from("abbbc")));
     }
+
+    #[test]
+    fn contains_three_of_any_letter_() {
+        assert!(contains_three_of_any_letter(&String::from("abcdddxx")));
+        assert!(!contains_three_of_any_letter(&String::from("abcddddddxx")));
+    }
+
+    #[test]
+    fn common_chars_() {
+        assert_eq!("", common_chars(&String::from("abc"), &String::from("edfg")));
+        assert_eq!("ac", common_chars(&String::from("abc"), &String::from("axc")));
+        assert_eq!("abcde", common_chars(&String::from("axbcde"), &String::from("aybcde")));
+    }
+
 }
