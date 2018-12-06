9c3dd4b9844dee6515b06c6df4d444ae79cb1618
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-06 20:36:59 +0100

    Learn about string splitting and trimming

--- aoc_3/src/main.rs
+++ aoc_3/src/main.rs
@@ -29,6 +29,12 @@ struct Rect {
     pub height: usize,
 }
 
+fn rect_from(_s: &String) -> Rect {
+    // let rect_part = s.split("@").collect()[0];
+
+    Rect {x: 0, y: 0, width: 0, height: 0}
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -46,5 +52,29 @@ mod tests {
         assert_eq!(2, r.y);
         assert_eq!(3, r.width);
         assert_eq!(4, r.height,);
+    }
+
+    #[test]
+    fn split_and_trim_strings() {
+        let s = "#1200 @ 94,536: 22x13";
+        let split: Vec<&str> = s.split("@").collect();
+        assert_eq!("#1200 ", split[0]);
+        assert_eq!(" 94,536: 22x13", split[1]);
+        assert_eq!("#1200", split[0].trim());
+        assert_eq!("94,536: 22x13", split[1].trim());
+    }
+
+
+    #[test]
+    #[ignore]
+    fn parse_rect_from_string() {
+        let s1 = String::from("#1200 @ 94,536: 22x13");
+        let parsed_rect = rect_from(&s1);
+
+        assert_eq!(94, parsed_rect.x);
+        assert_eq!(536, parsed_rect.y);
+        assert_eq!(22, parsed_rect.width);
+        assert_eq!(13, parsed_rect.height);
+
     }
 }
