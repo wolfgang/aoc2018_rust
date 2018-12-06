3bff81018d4666a8bed7c1fc8c2ab259bb06ce97
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-06 20:24:32 +0100

    Add implementation for Grid struct

--- aoc_3/src/main.rs
+++ aoc_3/src/main.rs
@@ -8,28 +8,27 @@ struct Grid {
     pub elements: Vec<i32>
 }
 
-fn grid_new(width: usize, height: usize) -> Grid {
-    Grid { width: width, height: height, elements: vec!(0; width*height) }
+impl Grid {
+    pub fn new(width: usize, height: usize) -> Grid {
+        Grid { width: width, height: height, elements: vec!(0; width*height) }
+    }
+
+    pub fn set(&mut self, x: usize, y: usize, value: i32)  {
+        self.elements[x + y * self.width] = value;
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> i32 {
+        self.elements[x + y*self.width]
+    }
 }
-
-fn grid_set(g: &mut Grid, x: usize, y: usize, value: i32) {
-    g.elements[x + y * g.width] = value;
-
-}
-
-fn grid_get(g: &Grid, x: usize, y: usize) -> i32 {
-    return g.elements[x + y * g.width];
-}
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
     fn grid() {
-        let mut g = grid_new(10, 20);
-        grid_set(&mut g, 5, 6, 1234);
-        assert_eq!(1234, grid_get(&g, 5, 6));
+        let mut g = Grid::new(10, 20);
+        g.set(5, 6, 1234);
+        assert_eq!(1234, g.get(5, 6));
     }
 }
