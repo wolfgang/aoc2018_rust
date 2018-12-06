817702a755bc167010769f950e4c86232b98f340
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-06 21:34:56 +0100

    Add function Grid::add_rect

--- aoc_3/src/main.rs
+++ aoc_3/src/main.rs
@@ -6,6 +6,24 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct Rect {
+    pub x: usize,
+    pub y: usize,
+    pub width: usize,
+    pub height: usize,
+}
+
+fn rect_from(rect_spec: &String) -> Rect {
+    let re = Regex::new(r"^#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)").unwrap();
+    let caps = re.captures(rect_spec).unwrap();
+    let x = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
+    let y = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+    let width = caps.get(3).unwrap().as_str().parse::<usize>().unwrap();
+    let height = caps.get(4).unwrap().as_str().parse::<usize>().unwrap();
+
+    Rect {x: x, y: y, width: width, height: height}
+}
+
 struct Grid {
     pub width: usize,
     pub height: usize,
@@ -24,25 +42,19 @@ impl Grid {
     pub fn get(&self, x: usize, y: usize) -> i32 {
         self.elements[x + y*self.width]
     }
+
+    pub fn add_rect(&mut self, rect: &Rect) {
+        for x in rect.x .. rect.x + rect.width {
+            for y in rect.y .. rect.y + rect.height {
+                let v = self.get(x, y);
+                if v < 2 {
+                    self.set(x, y, v+1)
+                }
+            }
+        }
+    }
 }
 
-struct Rect {
-    pub x: usize,
-    pub y: usize,
-    pub width: usize,
-    pub height: usize,
-}
-
-fn rect_from(rect_spec: &String) -> Rect {
-    let re = Regex::new(r"^#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)").unwrap();
-    let caps = re.captures(rect_spec).unwrap();
-    let x = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
-    let y = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
-    let width = caps.get(3).unwrap().as_str().parse::<usize>().unwrap();
-    let height = caps.get(4).unwrap().as_str().parse::<usize>().unwrap();
-
-    Rect {x: x, y: y, width: width, height: height}
-}
 
 #[cfg(test)]
 mod tests {
@@ -62,6 +74,46 @@ mod tests {
         assert_eq!(3, r.width);
         assert_eq!(4, r.height,);
     }
+
+    #[test]
+    fn add_rect_to_grid() {
+        let mut grid = Grid::new(3, 3);
+        let r1 = Rect {x: 1, y: 1, width: 2, height: 2};
+        grid.add_rect(&r1);
+        assert_eq!(0, grid.get(0, 0));
+        assert_eq!(0, grid.get(1, 0));
+        assert_eq!(0, grid.get(2, 0));
+        assert_eq!(0, grid.get(0, 1));
+        assert_eq!(1, grid.get(1, 1));
+        assert_eq!(1, grid.get(2, 1));
+        assert_eq!(0, grid.get(0, 2));
+        assert_eq!(1, grid.get(1, 2));
+        assert_eq!(1, grid.get(2, 2));
+    }
+
+    #[test]
+    fn add_rect_to_grid_overlap() {
+        let mut grid = Grid::new(2, 2);
+        let r1 = Rect {x: 0, y: 0, width: 2, height: 1};
+        grid.add_rect(&r1);
+        assert_eq!(1, grid.get(0, 0));
+        assert_eq!(1, grid.get(1, 0));
+        assert_eq!(0, grid.get(0, 1));
+        assert_eq!(0, grid.get(1, 1));
+
+        grid.add_rect(&r1);
+        assert_eq!(2, grid.get(0, 0));
+        assert_eq!(2, grid.get(1, 0));
+        assert_eq!(0, grid.get(0, 1));
+        assert_eq!(0, grid.get(1, 1));
+
+        grid.add_rect(&r1);
+        assert_eq!(2, grid.get(0, 0));
+        assert_eq!(2, grid.get(1, 0));
+        assert_eq!(0, grid.get(0, 1));
+        assert_eq!(0, grid.get(1, 1));
+    }
+
 
     #[test]
     fn split_and_trim_strings() {
@@ -85,20 +137,6 @@ mod tests {
         assert_eq!(text1, "123");
     }
 
-
-    #[test]
-    fn parse_rect_spec_with_regex() {
-        let rect_spec = "#1200 @ 94,536: 22x13";
-
-        let re = Regex::new(r"^#\d+\s@\s(\d+),(\d+):\s(\d+)x(\d+)").unwrap();
-        assert!(re.is_match(rect_spec));
-        let caps = re.captures(rect_spec).unwrap();
-        assert_eq!("94", caps.get(1).unwrap().as_str());
-        assert_eq!("536", caps.get(2).unwrap().as_str());
-        assert_eq!("22", caps.get(3).unwrap().as_str());
-        assert_eq!("13", caps.get(4).unwrap().as_str());
-    }
-
     #[test]
     fn rect_from_parses_rect_from_string() {
         let rect_spec = String::from("#128 @ 871,217: 11x29");
@@ -107,6 +145,6 @@ mod tests {
         assert_eq!(217, rect.y);
         assert_eq!(11, rect.width);
         assert_eq!(29, rect.height);
-
     }
+
 }
