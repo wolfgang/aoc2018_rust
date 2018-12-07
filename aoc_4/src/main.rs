010a84943fe1736e3f3438e8db1354913d66df07
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-07 19:19:12 +0100

    Save progress

--- aoc_4/src/main.rs
+++ aoc_4/src/main.rs
@@ -1,6 +1,8 @@
 extern crate regex;
 use regex::Regex;
 
+ use std::collections::HashMap;
+
 
 /* Part 1:
 - Create map day -> guard id
@@ -25,8 +27,13 @@ impl<'a> GuardFinder<'a> {
         return GuardFinder { input: input};
     }
 
+    pub fn days_to_guards(&self) -> HashMap<String, i32> {
+        let mut dtg = HashMap::new();
+        dtg.insert(String::from("1518-11-05"), 99);
+        return dtg;
+    }
+
     pub fn sleepiest_guard(&self) -> i32 {
-        
         return 0;
     }
 }
@@ -45,8 +52,28 @@ mod tests {
     }
 
     #[test]
+    fn guard_finder_days_to_guards() {
+        let input = part1_input();
+        let gc = GuardFinder::new(&input);
+
+        let days_to_guards = gc.days_to_guards();
+
+        assert_eq!(99, days_to_guards["1518-11-05"]);
+
+
+    }
+
+    #[test]
+    #[ignore]
     fn part1() {
-        let input = vec![
+        let input = part1_input();
+
+        let gc = GuardFinder::new(&input);
+        assert_eq!(10*24, gc.sleepiest_guard());
+    }
+
+    fn part1_input() -> Vec<String> {
+        vec![
             String::from("[1518-11-05 00:45] falls asleep"),
             String::from("[1518-11-01 00:05] falls asleep"),
             String::from("[1518-11-01 00:25] wakes up"),
@@ -64,9 +91,6 @@ mod tests {
             String::from("[1518-11-03 00:05] Guard #10 begins shift"),
             String::from("[1518-11-03 00:24] falls asleep"),
             String::from("[1518-11-05 00:55] wakes up")
-        ];
-
-        let gc = GuardFinder::new(&input);
-        assert_eq!(10*24, gc.sleepiest_guard());
+        ]
     }
 }
