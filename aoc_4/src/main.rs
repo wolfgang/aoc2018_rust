3e572bc4331ac1ceca9f059d968069b9f03999fa
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-07 08:31:43 +0100

    Experiment with parsing a timestamp using regex

--- aoc_4/src/main.rs
+++ aoc_4/src/main.rs
@@ -1,11 +1,20 @@
+extern crate regex;
+use regex::Regex;
+
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn can_assert() {
-        assert_eq!(1, 1);
+    fn parse_day_from_entry() {
+        let entry1 = "[1518-10-25 00:29]";
+        let re = Regex::new(r"^\[(\d{4}\-\d{2}\-\d{2})").unwrap();
+        let caps = re.captures(entry1).unwrap();
+        let day = caps.get(1).unwrap().as_str();
+        assert_eq!("1518-10-25", day);
+
     }
 }
