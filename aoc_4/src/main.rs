3054bfd4c6ed057c10ad87d3703ff6a2ac089476
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-07 20:28:26 +0100

    Verify that entries can be sorted by only comparing their strings

--- aoc_4/src/main.rs
+++ aoc_4/src/main.rs
@@ -1,17 +1,7 @@
 extern crate regex;
 use regex::Regex;
+use std::cmp::Ordering;
 
- use std::collections::HashMap;
-
-
-/* Part 1:
-- Create map day -> guard id
-- Create map day -> (guard id -> sleep/wake entries )
-- Parse above map to generate total minutes asleep
-    - sort sleep/awake entries by minute
-    - figure out minutes asleep per day
-    - sum up per guard
-*/
 
 fn main() {
     println!("Hello, world!");
@@ -32,6 +22,10 @@ impl<'a> GuardFinder<'a> {
     }
 }
 
+fn sort_chronologically(input: &Vec<String>) -> Vec<String> {    
+    return Vec::new();
+}
+
 fn parse_day_from_entry(entry : &String) -> String {
     let re = Regex::new(r"^\[(\d{4}\-\d{2}\-\d{2})").unwrap();
     let caps = re.captures(entry).unwrap();
@@ -46,8 +40,32 @@ fn parse_guard_from_entry(entry: &String) -> i32 {
     }
     return -1;
 }
+
+fn parse_time_values_from_entry(entry: &String) -> (i32, i32, i32, i32) {
+    let re = Regex::new(r"^\[\d{4}\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})").unwrap();
+    let caps = re.captures(entry).unwrap();
+    let month = caps.get(1).unwrap().as_str().parse::<i32>().unwrap();
+    let day = caps.get(2).unwrap().as_str().parse::<i32>().unwrap();
+    let hour = caps.get(3).unwrap().as_str().parse::<i32>().unwrap();
+    let minute = caps.get(4).unwrap().as_str().parse::<i32>().unwrap();
+
+    return (month, day, hour, minute);
+}
+
+fn compare_entries(entry1: &String, entry2: &String) -> Ordering {
+    let (month1, day1, hour1, minute1) = parse_time_values_from_entry(entry1);
+    let (month2, day2, hour2, minute2) = parse_time_values_from_entry(entry2);
+
+    let v1 = ((month1 * 30 + day1)*24 + hour1)*60 + minute1;
+    let v2 = ((month2 * 30 + day2)*24 + hour2)*60 + minute2;
+
+    return v1.cmp(&v2);
+}
  
 #[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
 mod tests {
     use super::*;
     #[test]
@@ -59,10 +77,26 @@ mod tests {
     }
 
     #[test]
+    fn parse_time_values_from_entry_() {
+        assert_eq!((10, 25, 0, 29), parse_time_values_from_entry(&String::from("[1518-10-25 00:29] Guard #1299 begins shift")));
+
+    }
+
+    #[test]
     fn parse_guard_from_entry_() {
         assert_eq!(-1, parse_guard_from_entry(&String::from("[1518-10-25 00:29] falls asleep")));
         assert_eq!(99, parse_guard_from_entry(&String::from("[1518-10-25 00:29] Guard #99 begins shift")));
         assert_eq!(1299, parse_guard_from_entry(&String::from("[1518-10-25 00:29] Guard #1299 begins shift")));
+    }
+
+
+    #[test]
+    fn sort_chronologically_() {
+        let mut input = part1_input();
+        input.sort();
+        // input.sort_by(|a, b| compare_entries(a, b));
+
+        assert_eq!(part1_input_chronological(), input);
     }
 
     #[test]
@@ -86,8 +120,8 @@ mod tests {
             String::from("[1518-11-02 00:40] falls asleep"),
             String::from("[1518-11-04 00:02] Guard #99 begins shift"),
             String::from("[1518-11-04 00:36] falls asleep"),
-            String::from("[1518-11-01 00:55] wakes up"),
             String::from("[1518-11-01 23:58] Guard #99 begins shift"),
+            String::from("[1518-11-01 00:55] wakes up"),
             String::from("[1518-11-02 00:50] wakes up"),
             String::from("[1518-11-03 00:29] wakes up"),
             String::from("[1518-11-03 00:05] Guard #10 begins shift"),
@@ -95,4 +129,27 @@ mod tests {
             String::from("[1518-11-05 00:55] wakes up")
         ]
     }
+
+    fn part1_input_chronological() -> Vec<String> {
+        vec![
+            String::from("[1518-11-01 00:00] Guard #10 begins shift"),
+            String::from("[1518-11-01 00:05] falls asleep"),
+            String::from("[1518-11-01 00:25] wakes up"),
+            String::from("[1518-11-01 00:30] falls asleep"),
+            String::from("[1518-11-01 00:55] wakes up"),
+            String::from("[1518-11-01 23:58] Guard #99 begins shift"),
+            String::from("[1518-11-02 00:40] falls asleep"),
+            String::from("[1518-11-02 00:50] wakes up"),
+            String::from("[1518-11-03 00:05] Guard #10 begins shift"),
+            String::from("[1518-11-03 00:24] falls asleep"),
+            String::from("[1518-11-03 00:29] wakes up"),
+            String::from("[1518-11-04 00:02] Guard #99 begins shift"),
+            String::from("[1518-11-04 00:36] falls asleep"),
+            String::from("[1518-11-04 00:46] wakes up"),
+            String::from("[1518-11-05 00:03] Guard #99 begins shift"),
+            String::from("[1518-11-05 00:45] falls asleep"),
+            String::from("[1518-11-05 00:55] wakes up")
+        ]
+    }
+
 }
