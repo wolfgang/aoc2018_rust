9801ce9795164a76568e8f730913415bb36ca36f
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-08 14:38:37 +0100

    Fun with rust strings and indexing them

--- aoc_5/src/main.rs
+++ aoc_5/src/main.rs
@@ -6,10 +6,38 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn react_polymer(polymer: &String) -> String {
+    let mut result = String::from("");
+
+    // for (i, c) in polymer.char_indices() {
+    //     if i>0 && polymer.get(i-1).as_str().to_uppercase() == c.as_str() {
+    //         return result;
+    //     }
+    // }
+
+    return polymer.clone();
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn can_assert() {
-        assert_eq!(1, 1);
+    fn single_step_reactions() {
+        assert_eq!("abcd", react_polymer(&String::from("abcd")));
+        // assert_eq!("", react_polymer(&String::from("aA")));
+    }
+
+    #[test]
+    fn indexing_strings() {
+        let s = String::from("abcd");
+        assert_eq!('a', s.chars().nth(0).unwrap());
+        assert_eq!('d', s.chars().nth(3).unwrap());
+    }
+
+    #[test]
+    fn chars() {
+        assert_eq!("a", 'a'.to_string());
+        assert_eq!('A', 'a'.to_ascii_uppercase());
+        assert_eq!('b', 'B'.to_ascii_lowercase());
     }
 }
