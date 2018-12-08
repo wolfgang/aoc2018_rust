213e0205f0100c79989cc2c4221ac99d5fd9e658
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-08 14:15:27 +0100

    Begin Day 5

---
+++ aoc_5/src/main.rs
@@ -0,0 +1,15 @@
+#[cfg(test)]
+#[macro_use]
+extern crate pretty_assertions;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn can_assert() {
+        assert_eq!(1, 1);
+    }
+}
