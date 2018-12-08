c791c3f80d557a039750494e564ec0b887e77efb
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-08 17:02:58 +0100

    Finish Day 5 Part 1

--- aoc_5/src/main.rs
+++ aoc_5/src/main.rs
@@ -2,8 +2,27 @@
 #[macro_use]
 extern crate pretty_assertions;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let polymer = read_input();
+
+    println!("Calculating part 1 answer ..");
+    assert_eq!(9154, get_length_of_reduced(&polymer));
+    println!("SUCCESS!!");
+}
+
+fn read_input() -> String {
+    let mut f = File::open("input.txt").expect("Failed to open input.txt");
+    let mut polymer = String::new();
+    f.read_to_string(&mut polymer).expect("could not read contents of input.txt");
+    return polymer;
+}
+
+fn get_length_of_reduced(polymer: &String) -> usize {
+    let reduced = reduce_polymer(&polymer);
+    return reduced.len();
 }
 
 fn reduce_polymer(polymer: &String) -> String {
@@ -18,14 +37,16 @@ fn reduce_polymer(polymer: &String) -> String {
 }
 
 fn react_polymer(polymer: &String) -> String {
-    let mut result = String::from("");
+    let mut result = String::with_capacity(polymer.len());
 
     let mut i = 0;
 
+    let bytes = polymer.as_bytes(); 
+
     while i < polymer.len() {
-        let current_char = char_at(i, polymer);
+        let current_char = bytes[i] as char;
         if i < polymer.len() - 1 {
-            let next_char = char_at(i+1, polymer);
+            let next_char = bytes[i+1] as char;
             if is_reacting(current_char, next_char) {
                 i +=2;
                 continue;
@@ -36,10 +57,6 @@ fn react_polymer(polymer: &String) -> String {
     }
 
     return result;
-}
-
-fn char_at(index: usize, s: &String) -> char {
-    s.chars().nth(index).unwrap()
 }
 
 fn is_reacting(c1: char, c2: char) -> bool {
@@ -61,7 +78,7 @@ mod tests {
      }
 
      #[test]
-     fn reduce_polymer() {
+     fn reduce_polymer_() {
         let polymer = String::from("dabAcCaCBAcCcaDA");
         assert_eq!("dabCBAcaDA", reduce_polymer(&polymer));
 
@@ -81,4 +98,14 @@ mod tests {
         assert_eq!('A', 'a'.to_ascii_uppercase());
         assert_eq!('b', 'B'.to_ascii_lowercase());
     }
+
+    #[test]
+    fn string_as_bytes() {
+        let s = String::from("abcd");
+        let b = s.as_bytes();
+        assert_eq!('a', b[0] as char);
+        assert_eq!('b', b[1] as char);
+    }
+
+
 }
