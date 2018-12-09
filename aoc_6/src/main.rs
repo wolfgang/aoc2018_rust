6814825dc769644a67b90e27521e94e134c9da9e
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-09 11:16:49 +0100

    Implement manhatten distance

--- aoc_6/src/main.rs
+++ aoc_6/src/main.rs
@@ -7,10 +7,21 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn md(c1 : (i32, i32), c2: (i32, i32)) -> u32 {
+    let (x1, y1) = c1;
+    let (x2, y2) = c2;
+
+    return ((x2 - x1).abs() + (y2 - y1).abs()) as u32;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn can_assert() {
-        assert_eq!(1 , 1);
+    fn manhatten_distance() {
+        assert_eq!(0,  md((0, 0), (0, 0)));
+        assert_eq!(31,  md((1, 2), (14, 20)));
+        assert_eq!(31,  md((14, 20), (1, 2)));
     }
 }
