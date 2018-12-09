4f605134af094328b5676a98a8f2a72f448efeee
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-09 11:20:15 +0100

    Start implementing an Area struct

--- aoc_6/src/main.rs
+++ aoc_6/src/main.rs
@@ -14,6 +14,17 @@ fn md(c1 : (i32, i32), c2: (i32, i32)) -> u32 {
     return ((x2 - x1).abs() + (y2 - y1).abs()) as u32;
 }
 
+struct Area {
+    coordinates: Vec<(i32, i32)>
+}
+
+impl Area {
+    fn new(initial_coord: (i32, i32)) -> Area {
+        Area {coordinates: vec![initial_coord]}
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -23,5 +34,11 @@ mod tests {
         assert_eq!(0,  md((0, 0), (0, 0)));
         assert_eq!(31,  md((1, 2), (14, 20)));
         assert_eq!(31,  md((14, 20), (1, 2)));
+    }
+
+    #[test]
+    fn initialize_area() {
+        let area = Area::new((1, 2));
+        assert_eq!(vec![(1, 2)], area.coordinates);
     }
 }
