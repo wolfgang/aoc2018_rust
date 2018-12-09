00156d23f1c4fffa85c8006fcd0c6c1ec2586f01
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-09 14:54:36 +0100

    Verify that HashMap works with tuple keys

--- aoc_6/src/main.rs
+++ aoc_6/src/main.rs
@@ -2,6 +2,8 @@
 #[macro_use]
 extern crate pretty_assertions;
 
+use std::collections::HashMap;
+
 type Coord = (i32, i32);
 
 fn main() {
@@ -34,7 +36,7 @@ fn find_nearest_coordinates_of(x: i32, y: i32, coordinates: &Vec<Coord>) -> Vec<
     let mut all_nearest : Vec<(u32, Coord)> = Vec::new();
     let mut nearest_distance = 99999;
     for coord in coordinates {
-        if *coord!=(x, y) && md((x, y), *coord) <= nearest_distance {
+        if md((x, y), *coord) <= nearest_distance {
             nearest_distance = md((x, y), *coord);
             all_nearest.push((nearest_distance, *coord));
         }
@@ -72,6 +74,10 @@ mod tests {
     fn find_nearest_coordinates_of_a_point() {
         let coords = vec![(0, 0), (2, 2)];
 
+        let nearest = find_nearest_coordinates_of(0, 0, &coords);
+        assert_eq!(1, nearest.len());
+        assert_eq!((0, 0), nearest[0]);
+
         let nearest = find_nearest_coordinates_of(1, 0, &coords);
         assert_eq!(1, nearest.len());
         assert_eq!((0, 0), nearest[0]);
@@ -105,7 +111,15 @@ mod tests {
             .map(|(_, s)| s)
             .collect();
         assert_eq!(vec!["coord2", "coord3"], result);
+    }
 
+    #[test]
+    fn hashmap_with_tuple_key() {
+        let mut hm : HashMap<(i32, i32), i32> = HashMap::new();
+        hm.insert((1, 2), 12);
+        hm.insert((3, 4), 34);
+        assert_eq!(12, *hm.get(&(1, 2)).unwrap());
+        assert_eq!(34, *hm.get(&(3, 4)).unwrap());
     }
 }
 
