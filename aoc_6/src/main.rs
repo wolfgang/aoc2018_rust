abc920a10c82f88ac5ae210229ebf22b165fbf95
wolfgang <wolfgang@wolfgangdeutsch.work>
2018-12-09 11:56:59 +0100

    Experiment with a struct AreaBuilder

--- aoc_6/src/main.rs
+++ aoc_6/src/main.rs
@@ -5,7 +5,7 @@ extern crate pretty_assertions;
 type Coord = (i32, i32);
 
 fn main() {
-    println!("Hello, world!");
+    panic!("No solutions provided");
 }
 
 fn md(c1 : Coord, c2: Coord) -> u32 {
@@ -15,6 +15,7 @@ fn md(c1 : Coord, c2: Coord) -> u32 {
     return ((x2 - x1).abs() + (y2 - y1).abs()) as u32;
 }
 
+#[derive(Debug, PartialEq)]
 struct Area {
     coordinates: Vec<Coord>
 }
@@ -28,6 +29,28 @@ impl Area {
         self.coordinates.push(coord);
     }
 }
+
+struct AreaBuilder {
+    width: u32,
+    height: u32,
+    areas: Vec<Area>
+}
+
+impl AreaBuilder {
+    fn new(width: u32, height: u32) -> AreaBuilder {
+        AreaBuilder {
+            width: width, 
+            height: height,
+            areas: Vec::new()
+        }
+    }
+
+    fn build(&self, coords: &Vec<Coord>) -> Vec<Area> {
+        vec![Area::new(coords[0])]
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -53,4 +76,24 @@ mod tests {
         area.add_coordinate((7, 8));
         assert_eq!(vec![(3, 4), (5, 6), (7, 8)], area.coordinates);
     }
+
+    #[test]
+    fn initialize_area_builder() {
+        let area_builder = AreaBuilder::new(100, 200);
+        assert_eq!(100, area_builder.width);
+        assert_eq!(200, area_builder.height);
+        assert_eq!(Vec::<Area>::new(), area_builder.areas);
+    }
+
+    #[test]
+    fn build_area_for_one_coord() {
+        let area_builder = AreaBuilder::new(1, 1);
+        let areas = area_builder.build(&vec![(0, 0)]);
+        assert_eq!(1, areas.len());
+        assert_eq!(1, areas[0].coordinates.len());
+        assert_eq!((0, 0), areas[0].coordinates[0]);
+
+    }
+
+
 }
